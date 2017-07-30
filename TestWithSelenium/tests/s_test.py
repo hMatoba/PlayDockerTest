00dc79b792eaece9d79c35d0a7f9d0205cde600d
hMatoba <hiroaki.mtb@outlook.com>
2017-07-30 10:48:09 +0900

    a

--- TestWithSelenium/tests/s_test.py
+++ TestWithSelenium/tests/s_test.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+#HOST = "http://172.17.0.1"
 HOST = "http://tornadoapp"
 
 class BrowserTests(unittest.TestCase):
